e18d09ecfb4751aadb875810881e8e4b35021007
AbelFierro <abelfierro@gmail.com>
2024-07-23 18:36:11 +0000

    auto commit 1       20240723 183610

--- src/workflows/619_workflow_base_f202109_test.r
+++ src/workflows/619_workflow_base_f202109_test.r
@@ -266,13 +266,12 @@ TS_strategy_base9 <- function( pinputexps )
   param_local$final_train$undersampling <- 1.0
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(202107, 202106, 202105, 202104, 202103, 202102,
-    202101, 202012, 202011,202010,202009,202008,202007)
+    202101, 202012, 202011)
 
 
-  param_local$train$training <- c(202105, 202104, 202103, 202102, 202101,
-    202012, 202011, 202010, 202009,202008,202007,202005,202004)
+  param_local$train$training <- c(202105,202104,202103,202101,202012,202002,202001,201911,201910)
   param_local$train$validation <- c(202106)
-  param_local$train$testing <- c(202107)
+  param_local$train$testing <- c(202107,202102,202011,201912)
 
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
